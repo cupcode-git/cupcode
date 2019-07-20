0b452b2bd8b8ba6e5d2f5f2e03ae4696a065b104
bruno <Brunouasli@cupcode.com.br>
2019-07-19 22:44:02 -0300

    adicional

--- header.php
+++ header.php
@@ -1,43 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="theme-color" content="#0642a3"/>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<title>Synx</title>
-
-<!-- main styling file  -->
-<link rel="stylesheet" href="css/site.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/gradient.css">
-<link rel="icon" href="images/favicon.png">
-
-<script src="https://kit.fontawesome.com/965317de7b.js"></script>
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-</head>
-<body data-spy="scroll" data-target=".scrolly" data-offset="50">
-
-
-<!--Preloader-->
-<div class="loader">
-   <div class="loader-inner">
-      <div class="spinner-eff spinner-eff-4">
-         <div class="bar bar-top"></div>
-         <div class="bar bar-right"></div>
-         <div class="bar bar-bottom"></div>
-         <div class="bar bar-left"></div>
-      </div>
-   </div>
-</div>
-<!-- Preloader ends-->
+<!DOCTYPE html> 
+<html lang="pt_BR"> 
+<head> 
+    <meta charset="utf-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="theme-color" content="#0642a3" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags --> 
+    <title>Cupcode</title> 
+ 
+    <!-- main styling file  --> 
+	<script src="https://kit.fontawesome.com/965317de7b.js"></script> 
+    <link rel="stylesheet" href="css/site.css"> 
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="css/mobile.css"> 
+    <link rel="stylesheet" href="css/typo1.css"> 
+    <link rel="icon" href="images/favicon.png"> 
+ 
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries --> 
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// --> 
+    <!--[if lt IE 9]> 
+              <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script> 
+              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> 
+    <![endif]--> 
+</head> 
+<body data-spy="scroll" data-target=".scrolly" data-offset="50"> 
+  <!-- Google Analytics --> 
+  <!-- Global site tag (gtag.js) - Google Analytics --> 
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141037054-1"></script> 
+  <script> 
+    window.dataLayer = window.dataLayer || []; 
+    function gtag(){dataLayer.push(arguments);} 
+    gtag('js', new Date()); 
+ 
+    gtag('config', 'UA-141037054-1'); 
+  </script> 
+  <!-- /Google Analytics --> 
+ 
+<!--Preloader--> 
+<div class="loader"> 
+   <div class="loader-inner"> 
+      <div class="spinner-eff spinner-eff-4"> 
+         <div class="bar bar-top"></div> 
+         <div class="bar bar-right"></div> 
+         <div class="bar bar-bottom"></div> 
+         <div class="bar bar-left"></div> 
+      </div> 
+   </div> 
+</div> 
+<!-- Preloader ends--> 
 <div class="wrapper">
 
 <!-- Main-Navigation -->
@@ -61,7 +71,7 @@
                   <div id="fixed-collapse-navbar" class="navbar-collapse collapse scrolly">
                      <div class="navbar-left-1">
                         <ul class="nav navbar-nav navbar-left">
-                           <li class="static"><a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">Home</a></li>
+                           <li><a href="index.php">home</a></li>
                            <li><a class="scrollings" href="marketing-digital.php">marketing</a></li>
                            <li><a class="scrollings" href="websites.php">websites</a> </li>
                            <li><a class="scrollings" href="#our-portfolio">Work</a></li>
