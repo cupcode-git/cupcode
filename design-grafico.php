6eecb8b406a803c719794132bd2a4c214c7c2673
bruno <Brunouasli@cupcode.com.br>
2019-08-23 13:44:04 -0300

    1.5

--- design-grafico.php
+++ design-grafico.php
@@ -1,15 +1,13 @@
 <?php $title = 'cupcode'; include("header.php"); ?>
 <!--Ful Screen hero Banner-->
-<section class="no-padding wow fadeIn no-transition">
+<section class="no-padding wow bg-hero-color fadeIn no-transition">
    <div id="rev_single_wrapper" class="rev_slider_wrapper fullscreen-container rev-slider" data-alias="inspiration-header" data-source="gallery" style="background-color:transparent;padding:0px;">
        <!-- start revolution slider 5.4.1 fullscreen mode -->
        <div id="rev_single" class="rev_slider fullscreenbanner" style="display:none;" data-version="5.4.1">
            <ul>	<!-- slide -->
                <li data-index="rs-layers" data-transition="fade" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="500"  data-rotate="0"  data-saveperformance="off"  data-title="Slide" data-param1="" data-param2="" data-param3="" data-param4="" data-param5="" data-param6="" data-param7="" data-param8="" data-param9="" data-param10="" data-description="">
                    <!-- main image -->
-                   <img src="images/transparent.png" data-bgcolor="#e0e0e0" alt=""  data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="off" class="rev-slidebg" data-no-retina>
                    <!-- layers -->
-
                    <!-- layer nr. 1 -->
                    <div class="tp-caption tp-resizeme rs-parallaxlevel-20"
                         id="layers-one"
