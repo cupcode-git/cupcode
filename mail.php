549e7f2bf1bef33a30207de9cd7f56a6364ca96c
Bruno <Brunouasli@gmail.com>
2019-11-25 17:58:19 -0300

    Correção envio de email

--- mail.php
+++ mail.php
@@ -1,27 +1,16 @@
 <?php
-    $to = 'contato@cupcode.com.br';
-    $name = $_REQUEST["name"];
-    $email= $_REQUEST["email"];
-    $fone= $_REQUEST["fone"];
-    $message= $_REQUEST["message"];
-    
+if(isset( $_POST['Nome']))      // Vai adicionar o elemento
+$name = $_POST['Nome'];         // Vai enviar o elemento
+if(isset( $_POST['E-mail']))     // Vai adicionar o elemento
+$email = $_POST['E-mail'];       // Vai enviar o elemento
+if(isset( $_POST['Telefone']))      // Vai adicionar o elemento
+$fone = $_POST['Telefone'];         // Vai enviar o elemento
+if(isset( $_POST['Mensagem']))   // Vai adicionar o elemento
+$message = $_POST['Mensagem'];   // Vai enviar o elemento
 
-
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From:" . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $text ='<table style="width:100%">
-        <tr><td>Nome: '.$name.'</td></tr>
-        <tr><td>E-mail: '.$email.'</td></tr>
-        <tr><td>Mensagem: '.$message.'</td></tr>
-        <tr><td>Numero: '.$fone.'</td></tr>
-    </table>';
-   
-        if (@mail($to, $email, $headers, $text)) {
-            echo 'Sua mensagem foi enviada com sucesso.';
-        }else{
-            echo 'Oh oh, deu um erro e sua mensagem não pode ser enviada. Tente novamente mais tarde.';
-        }
+$content="From: $name \n E-mail: $email \n Telefone: $fone \n Mensagem: $message"; // Vai fazer a leitura dos elementos dentro do conteúdo
+$recipient = "contato@cupcode.com.br"; // Vai enviar para o remetente que se encontra dentro do $recipient
+$mailheader = "From: $email \r\n"; // Vai declarar para onde vai ser feito o envio
+mail($recipient, $subject, $content, $mailheader) or die("Falha ao fazer o envio dos dados, tente novamente mais tarde!"); // Vai setar erro se tiver erro
+echo "Seus dados foram enviados com sucesso!"; // Vai enviar com sucesso se não tiver nenhum erro
 ?>
-
