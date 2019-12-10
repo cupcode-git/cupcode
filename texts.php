bd8615c71196185e26b45759a715cbf05c3f447a
Bruno <Brunouasli@gmail.com>
2019-12-10 01:38:26 -0300

    shell marketing digital

--- texts.php
+++ texts.php
@@ -29,6 +29,32 @@
     $criacontedit = "criação de conteúdo editorial";
     $portfolio = "portfólio";
     $todos = "todos";
+    $gerenciamento = "gerenciamento";
+    $post = "post";
+    $impulsionar = "impulsionar";
+    $googleads = "google ads";
+    $facebookads = "facebook ads";
+    $otimizacaodeconteudo = "otimização de conteúdo";
+    $seo = "seo";
+    $indicadordedesempenho = "indicador de desempenho";
+    $kpi = "kpi";
+    $maladireta = "mala direta";
+    $emailmarketing = "e-mail marketing";
+    $pacote = "pacote";
+    $pacotes = "pacotes";
+    $ristreto = "ristreto";
+    $lungo = "lungo";
+    $pctpersonalizado = "pacotes<br>personalizados";
+    $pct_mkt_digital_1 = "posts em redes sociais";
+    $pct_mkt_digital_2 = "anuncios no facebook ads";
+    $pct_mkt_digital_3 = "e-mail marketing publico alvo";
+    $pct_mkt_digital_4 = "análize de KPI";
+    $pct_mkt_digital_5 = "todo o conteúdo do pacote ristreto +";
+    $pct_mkt_digital_6 = "gerenciamento de redes sociais";
+    $pct_mkt_digital_7 = "anuncios no google ads";
+    $pct_mkt_digital_8 = "e-mail marketing para clientes ativos";
+    $pct_mkt_digital_9 = "adptação de conteúdo para SEO";
+    $pct_mkt_digital_10 = "análize de ROI (retorno de investimento)";
 
     /*=================================================================
                                 INDEX
@@ -74,5 +100,18 @@
                         INICIO - Portfolio
     ===================================================================*/
 
-
+    /*=================================================================
+                        inicio - Boxes marketing digital
+    ===================================================================*/
+    $descricaotxthero = "faça seu investimento, trabalhar por você!";
+    $descricaotxtredessociais = "as mídias sociais geram um grande impacto no mercado digital, são milhões de pessoas online a todo instante, só no brasil são mais de 127 milhões de pessoas conectadas, nosso serviço te coloca ao alcance desse público gigantesco.";
+    $descricaotxtgerenciamento = "cuidamos de suas redes sociais, desde seus posts até comentários e mensagens, incentivando interações e engajamento do seu público. sempre com o maior respeito a sua marca e seu principal bem, os clientes.";
+    $descricaotxtpost = "produzimos posts bem elaborados com conteúdos divertidos, sérios, emocionantes e envolventes que debatemos estrategicamente para levar a seu público o que você realmente quer passar.";
+    $descricaotxtimpulsionar = "não basta angariar curtidas e reações, na verdade esse é o principal ralo de dinheiro nas redes sociais, definir seu público-alvo, gerar leads e converter os leads em clientes é nossa especialidade.";
+    $descricaotxtgoogleads = "produzimos e gerenciamos os melhores anúncios para serem exibidos em outros sites pela internet. nossos profissionais analisam todos os dados em tempo real e fazem as modificações necessárias para que seu investimento produza um grande retorno.";
+    $descricaotxtfacebookads = "anunciar em redes sociais, é hoje, onde as empresas desperdiçam o maior volume de dinheiro. aqui na cupcode, agimos estrategicamente para garantir que o público alvo seja aquele que tem interesse pelos seus produtos/serviços e que você não pague apenas por likes.";
+    $descricaotxtseo = "a <strong>cupcode</strong> tem a garantia de que seu site estará nas primeiras páginas do google e outros buscadores, isso é possível por que somos especialistas em SEO de forma orgânica, trabalhando não só textos, mas posicionamento de itens e elementos para elevar seu site no ranking das pesquisas.";
+    $descricaotxtkpi = "com o kpi coletamos os dados de desempenho do seu negócio e depois de analisarmos vamos auxiliar com metas e formas de melhorar os resultados de sua empresa, sempre mantendo seu desempenho positivo.";
+    $descricaotxtemailmarketing = "podemos fazer propagandas com e-mail marketing, anunciando promoções, itens novos ou conteúdo de interesse do seu público-alvo, evitando que seus e-mails virem spam, com uma boa conduta, sempre respeitando os seus clientes.";
+    $descricaotxtpctpersonalizado = "quer um pacote para chamar de seu? criamos um pacote do seu jeito para suprir as suas necessidades. entre em contato agora mesmo e solicite uma personalização.";
 ?>
