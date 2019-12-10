f0652cf8b8d9f3c9c61c9d835642a62607ded477
Lauro Rogwe <lauroroger@icloud.com>
2019-12-10 00:03:46 -0300

    vvvv

---
+++ texts.php
@@ -0,0 +1,78 @@
+<?php
+
+  /*=================================================================
+                        Titulos para o site todo
+    ===================================================================*/
+
+    $marketing = "marketing";
+    $digital = "digital";
+    $mktdigital = "marketing digital";
+    $brading = "branding";
+    $web = "web";
+    $site = "site";
+    $sites = "sites";
+    $website = "website";
+    $websites = "websites";
+    $siteseblogs = "sites e blogs";
+    $redessociais = "redes sociais";
+    $design = "design";
+    $grafico = "gráfico";
+    $designgrafico = "design gráfico";
+    $papelaria = "papelaria";
+    $clickemais = "clique aqui e saiba mais sobre";
+    $hospedagem = "hospedagem";
+    $hospedagens = "hospedagens";
+    $anuncios = "anúncios";
+    $adapseo = "adaptação SEO";
+    $criacao = "criação";
+    $editorial = "editorial";
+    $criacontedit = "criação de conteúdo editorial";
+    $portfolio = "portfólio";
+    $todos = "todos";
+
+    /*=================================================================
+                                INDEX
+    ===================================================================*/
+    /*=================================================================
+                        INICIO - Boxes index
+    ===================================================================*/
+
+    $boxtxtsociais = "nossa equipe é treinada para tirar o melhor das redes sociais, vamos disponibilizar seu conteúdo para quem realmente precisa dele, diminuindo custos com posts impulsionados ou patrocinados ao agirmos de forma orgânica. além da retenção e manutenção constante dos seus clientes e seguidores,  utilização do Facebook Pixel e muito mais.";
+    $boxtxtanuncios = "para investir em anúncios hoje em dia e não perder dinheiro conquistando apenas curtidas, precisa-se ter muito conhecimento, estratégia e uma equipe focada em converter investimento em leads e clientes, e isso a cupcode tem! nós literalmente vamos fazer seu investimento trabalhar por você.";
+    $boxtxtsites = "não basta ter uma página na internet, você precisa ter um site funcional e \"visível\" aos mecanismos de buscas como o Google, Bing e outros mecanismos de pesquisa. Aqui na <strong>cupcode</strong>, temos especialistas que fazem sites dinâmicos, que chama a atenção e atraem clientes. sabemos exatamente o que seu cliente procura e nós oferecemos isso a ele, seu site de forma fácil e atraente para realmente mantê-lo na sua página. unidos a redes sociais, notícias que importam e conteúdo relevante que levam seu cliente a te procurar em busca de serviços e produtos, fazemos sites responsivos que se adaptam em todos os tamanhos de tela seja ela gigante ou uma tela tão pequena quanto a de seu celular, aproveitando o melhor de cada dispositivo.";
+    $boxtxthost = "não faltará terreno para sua casa, servidores de alto desempenho para seu site ficar sempre online, com performance de ponta e baixo custo, nossa hospedagem é até 10 vezes mais rápida que as hospedagens padrões e o melhor de tudo, você não se preocupa com absolutamente nada, pois toda a configuração e manutenção está sobre nossa responsabilidade. você só relaxa e aproveita seus clientes.";
+    $boxtxtbranding = "quer você tenha um logotipo que quer mudar ou melhorar, ou até mesmo esteja precisando de um novo, podemos fazer logo, manual da marca, paleta de cores, tipografia e tudo o que você precisa para sua identidade visual.";
+    $boxtxtpapelaria = "criamos sua arte para impressão ou digital, quer você precise de um cartão de visita, flyer, folder, sacola, cardápio, materiais de escritório, publicidade para jornais e revistas, catálogo, banner, outdoor ou qualquer outra arte, com alta qualidade e design que respeita sua identidade visual.";
+    $boxtxtadapseo = "adaptamos seu texto para tirar o melhor proveito nas pesquisas do google e outros mecanismos de buscas, elevando seu conteúdo as primeiras páginas nas pesquisas.";
+    $boxtxtcriaedit = "nós produziremos matérias com foco em SEO para melhorar o posicionamento no raking do google e outros mecanismos de buscas na internet, para colocar seu site nas primeiras páginas das pesquisas, com conteúdo escrito por especialistas no assunto, escreveremos artigos e matérias exclusivas para sua empresa, artigos com conteúdo relevante e específicos da sua área de atuação.";
+
+    /*=================================================================
+                        FIM - Boxes index
+    ===================================================================*/
+
+    /*=================================================================
+                        INICIO - Resumos dos boxes index
+    ===================================================================*/
+    $resumotxtsociais = "postagens, gerenciamento de conteúdo, comunicação e indução ao engajamento com clientes.";
+    $resumotxtanuncios = "anunciamos com inteligência para você ter o melhor retorno de seu investimento.";
+    $resumotxtsites = "criamos seu site, blog ou loja virtual de forma responsiva e intuitiva, leve e de fácil navegação.";
+    $resumotxthost = "temos a melhor configuração para sua empresa ter um site confiável, estável e funcional.";
+    $resumotxtbranding = "construímos toda a sua identidade visual, desde o logotipo até manual da marca.";
+    $resumotxtpapelaria = "fazemos todas suas lâminas para impressão ou digital, com qualidade e inteligência.";
+    $resumotxtadapseo = "adaptamos seu artigo para ser melhor visto por mecanismos de buscas na internet.";
+    $resumotxtcriaedit = "criação de conteúdo editorial, com foco em SEO, colocando seu site nas primeiras páginas do google.";
+    /*=================================================================
+                        FIM - Resumos dos boxes index
+    ===================================================================*/
+
+    /*=================================================================
+                        INICIO - Portfolio
+    ===================================================================*/
+
+
+    /*=================================================================
+                        INICIO - Portfolio
+    ===================================================================*/
+
+
+?>
